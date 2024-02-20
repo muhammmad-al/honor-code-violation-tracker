92150f929add6ede16e50728df0060bf862a0e12
tdg8nx <tdg8nx@virginia.edu>
2024-02-19 23:54:38 -0500

    Forgot to change database back from testing

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#        'default': dj_database_url.config(conn_max_age=600)
-#        }
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(conn_max_age=600)
         }
-    }
+
+#DATABASES = {
+#        'default': {
+ #           'ENGINE': 'django.db.backends.sqlite3',
+ #           'NAME': BASE_DIR / 'db.sqlite3',
+ #       }
+ #   }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
