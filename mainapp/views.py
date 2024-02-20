e811f61ffbe54e90fc7e89b4c554d51a34afd8be
tdg8nx <123898991+tdg8nx@users.noreply.github.com>
2024-02-20 05:52:56 +0100

    Revert "Loginscreenview"

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,17 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.views import View
-from allauth.socialaccount.models import SocialAccount
 
-
-def index(request):
-    return render(request, 'index.html')
-
-
-class UserLoginView(View):
-    def get(self, request):
-        return render(request, 'user_login.html')
-
-class AdminLoginView(View):
-    def get(self, request):
-        return render(request, 'admin_login.html')
+# Create your views here.
