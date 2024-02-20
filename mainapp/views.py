d0bf46dbf7d68f10e696e10fe559375606f14428
tdg8nx <tdg8nx@virginia.edu>
2024-02-19 23:49:34 -0500

    Redid paths and views and added admin login and user login htmls

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,17 +9,9 @@ def index(request):
 
 
 class UserLoginView(View):
-    template_name = 'user_login.html'
-
-    def get(self, request, *args, **kwargs):
-        context = {'provider_login_url': SocialAccount.get_provider('google').get_login_url(request, next=None, **{})}
-        return render(request, self.template_name, context)
-
+    def get(self, request):
+        return render(request, 'user_login.html')
 
 class AdminLoginView(View):
-    template_name = 'admin_login.html'
-
-    @login_required
-    def get(self, request, *args, **kwargs):
-        context = {'username': request.user.username}
-        return render(request, self.template_name, context)
+    def get(self, request):
+        return render(request, 'admin_login.html')
