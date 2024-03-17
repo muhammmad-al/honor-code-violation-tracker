d1a3a9863f666d1570108ad9176f122397df1df8
tdg8nx <tdg8nx@virginia.edu>
2024-03-17 17:52:59 -0400

    Settings stuff for S3

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,6 +158,12 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')  # Default to 'us-east-1' if not set
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 SITE_ID = 5 
 
